d69426dbda295bd73f4c6ea9bcf80a56124fa3da
Dominik Xaver Hörl <hoe.dom@gmx.de>
2016-11-30 22:19:50 +0100

    Added debug builtin for internal use.

---
+++ HFish/Interpreter/Builtins/Debug.hs
@@ -0,0 +1,17 @@
+{-# language LambdaCase, OverloadedStrings #-}
+module HFish.Interpreter.Builtins.Debug (
+  debugF
+) where
+
+import HFish.Interpreter.Core
+import HFish.Interpreter.IO
+import HFish.Interpreter.FdTable
+
+import Control.Monad
+import Control.Monad.IO.Class
+
+debugF :: Builtin
+debugF _ = \case
+  "fdtable":_ -> askFdTable >>= (liftIO . print)
+  _ -> return ()
+  

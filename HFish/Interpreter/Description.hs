942c26caeffcc3814bcd8baf1e44e77ddcf06edd
Dominik Xaver Hörl <hoe.dom@gmx.de>
2017-01-31 16:21:44 +0100

    Additions to Description.

--- HFish/Interpreter/Description.hs
+++ HFish/Interpreter/Description.hs
@@ -1,7 +1,31 @@
 {-# language OverloadedStrings #-}
-module HFish.Interpreter.Description where
+module HFish.Interpreter.Description (
+  description
+) where
 
+import Prelude hiding ((<$>))
 import Text.PrettyPrint.ANSI.Leijen
 
 description :: Doc
-description = "todo: hfish description"
+description =
+  "hfish is a fish-like shell."
+  </> " It has the following options:"
+  <> hang 4 options
+
+options :: Doc
+options = 
+  help
+  <$> version
+  <$> noexec
+  <$> ast
+  <$> compat
+  <$> cmd
+
+help = ""
+version = ""
+noexec = ""
+ast = ""
+compat = ""
+cmd = ""
+
+-- "-c / --command"

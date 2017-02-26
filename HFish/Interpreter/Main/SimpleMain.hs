62611aa10971e9662dc0e5f139d8f36d6fd1bfee
Dominik Xaver Hörl <hoe.dom@gmx.de>
2017-02-26 15:52:58 +0100

    Extend startup sourcing mechanism.

--- HFish/Interpreter/Main/SimpleMain.hs
+++ HFish/Interpreter/Main/SimpleMain.hs
@@ -1,6 +1,8 @@
 {-# language LambdaCase, OverloadedStrings, FlexibleInstances, ScopedTypeVariables #-}
 module HFish.Interpreter.Main.SimpleMain where
 
+import Debug.Trace
+
 import Control.Lens
 import Control.Monad
 import Control.Monad.IO.Class
@@ -12,6 +14,7 @@ import Data.Maybe
 import Data.List as L
 import Control.Exception (try)
 import qualified Data.Text as T
+import HFish.Interpreter.Util
 import HFish.Interpreter.Interpreter
 import HFish.Interpreter.Scope
 import HFish.Interpreter.Core
@@ -24,9 +27,12 @@ import Fish.Lang
 
 import System.Console.Haskeline
 import System.Environment (getArgs)
+import System.Directory (listDirectory)
+import System.FilePath (takeExtensions,(</>))
 
 import Options.Applicative as O
 import Options.Applicative.Builder as OB
+import Options.Applicative.Help.Types (ParserHelp)
 
 import Fish.Pretty
 import Text.PrettyPrint.GenericPretty (doc)
@@ -36,12 +42,17 @@ main :: IO ()
 main = execParserPure conf parser <$> getArgs
   >>= \case
     Success a -> a
-    Failure err -> putStrLn . fst $ renderFailure err ""
+    Failure e -> showError e
   where
+    showError :: ParserFailure ParserHelp -> IO ()
+    showError e = putStrLn . fst
+      $ renderFailure e ""
+    
     conf = OB.defaultPrefs {
       prefDisambiguate = True
       ,prefShowHelpOnError = True
     }
+    
     parser = info (helper <*> (versionOpt <|> hfishOptions))
       (fullDesc
         <> header "hfish: a fish-like shell, written in haskell"
@@ -122,9 +133,25 @@ injectArgs xs = runFish
 
 executeStartupFiles :: Bool
   -> FishReader -> FishState -> IO FishState
-executeStartupFiles fishCompat r s =
-  foldM tryExecute s files
+executeStartupFiles fishCompat r s = do
+  confFiles <- tryListDirectory confDir
+    <$$> (>>= prepare confDir)
+  funcFiles <- tryListDirectory funcDir
+    <$$> (>>= prepare funcDir)
+  foldM tryExecute s
+    $ pure "/etc/hfish/config.hfish"
+      ++ confFiles ++ funcFiles
   where
+    prepare path x = do
+      guard $ isHFishFile x
+      pure $ path </> x
+    
+    tryListDirectory :: FilePath -> IO [FilePath]
+    tryListDirectory path = try (listDirectory path)
+      <$$> \case
+        Left (e::IOException) -> []
+        Right r -> r
+    
     tryExecute :: FishState -> FilePath -> IO FishState
     tryExecute s path = try (parseFile fishCompat path)
       >>= \case
@@ -132,8 +159,12 @@ executeStartupFiles fishCompat r s =
         Right res -> withProg res (runProgram r s)
           >>= return . fromMaybe s
     
-    files = [ "/etc/hfish/config.hfish" ]
-    -- ^ todo: more elaborate logic here
+    
+    
+    isHFishFile p = takeExtensions p == ".hfish"
+    
+    confDir = "/etc/hfish/conf.d"
+    funcDir = "/etc/hfish/functions"
 
 mkPrompt :: Bool -> FishState -> String
 mkPrompt isbrkpt s
@@ -163,18 +194,13 @@ interpreterLoop fishCompat prompt r s =
         (runProgram r s)
       interpreterLoop fishCompat prompt r (fromMaybe s ms')
 
-coerce :: MonadIO io
-  => IO (Either SomeException a)
-  -> io (Either SomeException a)
-coerce = liftIO
-
 runProgram :: MonadIO io
   => FishReader
   -> FishState
   -> Prog T.Text ()
   -> io FishState
 runProgram r s p =
-  ( coerce . try $ runFish (progA p) r s ) >>= \case
+  ( liftIO . try $ runFish (progA p) r s ) >>= \case
     Left e -> do
       liftIO $ putStr (showError e)
       return s
